2b3e87c2373c48f6639b1148236515df68bf627a
MailsonAndradeCardoso <111311563+MailsonAndradeCardoso@users.noreply.github.com>
2022-08-30 15:08:34 -0300

    Update script.js
    corrigindo linha 9 e 10

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,18 +1,15 @@
-//Exercicio 2 (Aula a008)
-
-let primPergunta = prompt("Diga um numero!")
-let segPergunta = prompt("Diga outro numero por favor!")
+let primPergunta = Number(prompt("Diga um numero!"))
+let segPergunta = Number(prompt("Diga outro numero por favor!"))
 console.log(primPergunta)
 console.log(segPergunta)
 
-let a = primPergunta
-let b = segPergunta
-let c = a > b
-let d = a = b
-let e = a / b
-let f = a % b
 
-console.log(c)
-console.log(d)
-console.log(e)
-console.log(f)
+let numMaior = primPergunta > segPergunta
+let numIgual = primPergunta === segPergunta
+let divido1 = primPergunta % segPergunta === 0
+let divido2 = segPergunta % primPergunta ===
+
+console.log(numMaior)
+console.log(numIgual)
+console.log(divido1)
+console.log(divido2)
