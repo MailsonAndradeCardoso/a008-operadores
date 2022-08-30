f111cd2f91bda8a8211201054dec21bcc1d45201
MailsonAndradeCardoso <mailsoncardoso1988@gmail.com>
2022-08-30 11:21:14 -0300

    Exercicios ok

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+//Exercicio 2 (Aula a008)
+
+let primPergunta = prompt("Diga um numero!")
+let segPergunta = prompt("Diga outro numero por favor!")
+console.log(primPergunta)
+console.log(segPergunta)
+
+let a = primPergunta
+let b = segPergunta
+let c = a > b
+let d = a = b
+let e = a / b
+let f = a % b
+
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
